e4a6c35304d2bb7b446c5f80bc01f6ee97756bd2
fahmi ahmad <fahmiahmad@tutanota.com>
2022-04-16 16:35:37 +0700

    dockerized

--- rest-api/rest-prod/internal/comment/comment.go
+++ rest-api/rest-prod/internal/comment/comment.go
@@ -2,7 +2,13 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
 )
 
 /*
@@ -16,6 +22,10 @@ type Comment struct {
 	Author string
 }
 
+/*
+  Store -
+  defines all of the methods that our service needs to operate
+*/
 type Store interface {
 	GetComment(context.Context, string) (Comment, error)
 }
@@ -42,11 +52,27 @@ func NewService(store Store) *Service {
 func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	fmt.Println("retrieving a comment")
 
+	// ctx = context.WithValue(ctx, "request_id", "unique-string")
+
+	// fmt.Println(ctx.Value("request_id"))
+
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }

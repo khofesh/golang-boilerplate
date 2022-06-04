82434409ab7fd717029926dd55ae739f8ff3d163
fahmi ahmad <fahmiahmad@tutanota.com>
2022-06-04 16:36:17 +0700

    subtest

--- go-testing/calculator/calculator_test.go
+++ go-testing/calculator/calculator_test.go
@@ -12,25 +12,55 @@ type TestCase struct {
 }
 
 func TestCalculateIsAmstrong(t *testing.T) {
-	testCase := TestCase{
-		value:    371,
-		expected: true,
-	}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+	t.Run("should return true", func(t *testing.T) {
+		testCase := TestCase{
+			value:    371,
+			expected: true,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should return true for 370", func(t *testing.T) {
+		testCase := TestCase{
+			value:    370,
+			expected: true,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
 }
 
 func TestNegativeCalculateIsAmstrong(t *testing.T) {
-	testCase := TestCase{
-		value:    350,
-		expected: false,
-	}
+	t.Run("should fail for case 350", func(t *testing.T) {
+		testCase := TestCase{
+			value:    350,
+			expected: false,
+		}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should fail for case 300", func(t *testing.T) {
+		testCase := TestCase{
+			value:    300,
+			expected: false,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
 }
